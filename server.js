62d29d5f3ed19a2ca89bdd8d20d62cd46ebdff27
PauQuiriti <pauquiriti@hotmail.com>
2023-04-22 23:18:39 -0300

    change port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const routes = require('./routes')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 const uri = 'mongodb+srv://paula:ErvAOHTvdSMx4J7B@techtalk.yxwdryb.mongodb.net/?retryWrites=true&w=majority'
 
 const corsOptions = {
